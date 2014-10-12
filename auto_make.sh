cfce589d7e5d02cdd6240514f98d489e8d362647
X-s <xsmumu@gmail.com>
2014-10-12 23:34:53 +0800

    update auto_make.sh

--- auto_make.sh
+++ auto_make.sh
@@ -3,14 +3,15 @@
 #sh for miui patchrom
 #usage:
 #. make.sh $1 $2
-#$1 == BUILD_NUMBER
-#$2 == Last_Target.zip
+#$1 == Device
+#$2 == BUILD_NUMBER
+#$3 == Last_Target.zip
 echo "******************"
 echo "*Init Environment*"
 echo "******************"
 cd ..
 . build/envsetup.sh
-cd bacon
+cd $1
 pwd
 echo "******************"
 echo "*     Clean      *"
@@ -19,11 +20,11 @@ make clean
 echo "******************"
 echo "*     Fullota    *"
 echo "******************"
-make fullota BUILD_NUMBER=$1
+make fullota BUILD_NUMBER=$2
 echo "******************"
 echo "*Copy Fullota.zip*"
 echo "******************"
-cp out/fullota.zip $1-fullota.zip
+cp out/fullota.zip $2-fullota.zip
 echo "******************"
 echo "*      OTA       *"
 echo "******************"
@@ -31,8 +32,8 @@ echo "------------------"
 echo "    make ota?     "
 echo "------------------"
 read
-../tools/releasetools/ota_from_target_files -k ../build/security/testkey -i $2 out/target_files.zip OTA-$2-$1.zip
-cp out/target_files.zip $1-target.zip
+../tools/releasetools/ota_from_target_files -k ../build/security/testkey -i $3 out/target_files.zip OTA-$3-$2.zip
+cp out/target_files.zip $2-target.zip
 echo "******************"
 echo "*     Done!      *"
 echo "******************"
